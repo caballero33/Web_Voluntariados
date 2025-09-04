2e28d291a68289ed02a7c641cc3a9d4b1439ea63
IvanHHdz <hernivan336@outlook.com>
2025-09-04 17:04:14 -0600

    pumas verdes
    se implementó la página de pumas verdes
    se optimizó la sección de todos los voluntariados utilizando templates
    se mejoró el sistema de variables globales para los colores en el style.css

---
+++ Voluntariados/nuestros_voluntariados/static/js/pumas_verdes.js
@@ -0,0 +1,10 @@
+console.log("Pumas Verdes page loaded");
+    
+// Cambiar colores para el tema de Pumas Verdes
+document.documentElement.style.setProperty('--color-primary', '#00d1b2');
+document.documentElement.style.setProperty('--color-bg-dark', '#037766ff');
+document.documentElement.style.setProperty('--color-primary-dark', '#037766ff');
+
+document.documentElement.style.setProperty('--color-light-mode-primary', '#b8f3e6ff');
+document.documentElement.style.setProperty('--color-bg-light', '#d9f6eeff');
+document.documentElement.style.setProperty('--color-light-mode-bg-dark', '#60e0ccff');
--- Voluntariados/nuestros_voluntariados/urls.py
+++ Voluntariados/nuestros_voluntariados/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "nuestros_voluntariados"  # <-- Add this line
 
 urlpatterns = [
-    path("home/", views.index, name="home"),
+    path("home/", views.home, name="home"),
+    path("pumas_verdes/", views.pumas_verdes, name="pumas_verdes"),
 ]
