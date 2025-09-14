28e589f745b2ff83e31e8cf477701dd92800271f
IvanHHdz <hernivan336@outlook.com>
2025-09-14 10:50:09 -0600

    modularización
    se ha mejorado la modularización con más plantillas
    con el objetivo que la mayor cantidad de cosas posibles se renderizen por plantillas

--- Voluntariados/home/views.py
+++ Voluntariados/home/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.urls import reverse
 
 def home(request):
-    return render(request, 'home.html')
+    context = {
+        "url_pumas_verdes": reverse('nuestros_voluntariados:pumas_verdes'),
+        "url_sonriendo_juntos": reverse('nuestros_voluntariados:sonriendo_juntos'),
+        "url_patitas_unah": reverse('nuestros_voluntariados:patitas_unah'),
+    }
+
+    return render(request, 'home.html', context)
 
 def reglamento(request):
     return render(request, 'reglamento.html')
