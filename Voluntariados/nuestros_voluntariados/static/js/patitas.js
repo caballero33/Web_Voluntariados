81b0de499e394a37e71c22928023dfd4af03e7f3
IvanHHdz <hernivan336@outlook.com>
2025-09-14 08:59:13 -0600

    Página para Patitas UNAH

---
+++ Voluntariados/nuestros_voluntariados/static/js/patitas.js
@@ -0,0 +1,10 @@
+console.log("Pumas Verdes page loaded");
+    
+// Cambiar colores para el tema de Pumas Verdes
+document.documentElement.style.setProperty('--color-primary', '#FF7F50');
+document.documentElement.style.setProperty('--color-bg-dark', '#8B4513');
+document.documentElement.style.setProperty('--color-primary-dark', '#c2611bff');
+
+document.documentElement.style.setProperty('--color-light-mode-primary', '#f39069ff');
+document.documentElement.style.setProperty('--color-bg-light', '#ea9e80ff');
+document.documentElement.style.setProperty('--color-light-mode-bg-dark', '#ff8f66ff');
