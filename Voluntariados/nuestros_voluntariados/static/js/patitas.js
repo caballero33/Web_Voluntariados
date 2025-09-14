3380eb55739adb94c64d17226af879d8107d3d96
IvanHHdz <hernivan336@outlook.com>
2025-09-14 10:56:11 -0600

    correcciones menores
    correcciones ortográficas y mejoras de color

--- Voluntariados/nuestros_voluntariados/static/js/patitas.js
+++ Voluntariados/nuestros_voluntariados/static/js/patitas.js
@@ -3,7 +3,7 @@ console.log("Patitas UNAH page loaded");
 // Cambiar colores para el tema de Patitas UNAH
 document.documentElement.style.setProperty('--color-primary', '#FF7F50');          // Coral como color principal
 document.documentElement.style.setProperty('--color-primary-dark', '#8B4513');     // Chocolate como tono oscuro
-document.documentElement.style.setProperty('--color-bg-dark', '#FF6347');          // Tomato (más fuerte que coral)
+document.documentElement.style.setProperty('--color-bg-dark', '#ef4d31ff');          // Tomato (más fuerte que coral)
 
 document.documentElement.style.setProperty('--color-light-mode-primary', '#FFA07A');     // Light Salmon como versión clara
 document.documentElement.style.setProperty('--color-bg-light', '#FFD5C2');               // Suave, derivado de coral
--- Voluntariados/nuestros_voluntariados/static/js/sonriendo_juntos.js
+++ Voluntariados/nuestros_voluntariados/static/js/sonriendo_juntos.js
@@ -3,7 +3,7 @@ console.log("Sonriendo Juntos page loaded");
 // Cambiar colores para el tema de Sonriendo Juntos
 document.documentElement.style.setProperty('--color-primary', '#FFDE21');          // Amarillo brillante principal
 document.documentElement.style.setProperty('--color-primary-dark', '#E0BC00');     // Amarillo más oscuro/dorado
-document.documentElement.style.setProperty('--color-bg-dark', '#FFD32C');          // Amarillo intenso para fondos
+document.documentElement.style.setProperty('--color-bg-dark', '#deb214ff');          // Amarillo intenso para fondos
 
 document.documentElement.style.setProperty('--color-light-mode-primary', '#FFEA99');   // Amarillo claro para modo claro
 document.documentElement.style.setProperty('--color-bg-light', '#FFF6CC');             // Fondo suave, derivado del amarillo
