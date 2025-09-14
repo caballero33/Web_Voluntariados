371e9c6cd2623ff2bb8039ccbb008c14e688926b
IvanHHdz <hernivan336@outlook.com>
2025-09-14 09:01:58 -0600

    Añadidos para patitas unah y depuración del css

--- Voluntariados/nuestros_voluntariados/urls.py
+++ Voluntariados/nuestros_voluntariados/urls.py
@@ -7,4 +7,5 @@ app_name = "nuestros_voluntariados"  # <-- Add this line
 urlpatterns = [
     path("home/", views.home, name="home"),
     path("pumas_verdes/", views.pumas_verdes, name="pumas_verdes"),
+    path("patitas_unah/", views.patitas_unah, name="patitas_unah"),
 ]
