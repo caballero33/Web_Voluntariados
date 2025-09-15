b54ca9947e956c3ec1abc773bff99ed447253ebf
IvanHHdz <hernivan336@outlook.com>
2025-09-14 22:07:41 -0600

    reporte de errores

--- Voluntariados/home/urls.py
+++ Voluntariados/home/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("reglamento/", views.reglamento, name="reglamento"),
+    path("errores/", views.errores, name="errores"),
 ]
